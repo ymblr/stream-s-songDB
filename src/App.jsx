a27833f1dd8e7951d3be8e16263454cd4ec39335
ymblr <ymblr.work@gmail.com>
2026-04-17 00:13:19 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { PlayerProvider } from './contexts/PlayerContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 import Navigation from './components/Navigation';
 import MiniPlayer from './components/MiniPlayer';
 import Home from './pages/Home';
@@ -9,24 +10,25 @@ import Search from './pages/Search';
 import Playlists from './pages/Playlists';
 import PlaylistDetail from './pages/PlaylistDetail';
 
-// GitHub Pages SPA routing fix
 const basename = import.meta.env.BASE_URL;
 
 export default function App() {
   return (
-    <AuthProvider>
-      <PlayerProvider>
-        <BrowserRouter basename={basename}>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/playlists" element={<Playlists />} />
-            <Route path="/playlist/:id" element={<PlaylistDetail />} />
-          </Routes>
-          <MiniPlayer />
-        </BrowserRouter>
-      </PlayerProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <PlayerProvider>
+          <BrowserRouter basename={basename}>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/playlists" element={<Playlists />} />
+              <Route path="/playlist/:id" element={<PlaylistDetail />} />
+            </Routes>
+            <MiniPlayer />
+          </BrowserRouter>
+        </PlayerProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
